f1d25b6d10b2e9497a79b31fcdde31f6244542f6
jorgitsss <jorge.godoy.vergara@gmail.com.com>
2022-12-29 21:49:00 -0300

    Aleatorio que elije a un alumno entre el array y los grupos

--- alumnos.rb
+++ alumnos.rb
@@ -24,8 +24,17 @@ group_one   = alumnos[0..4]
 group_two   = alumnos[5..10]
 group_three = alumnos[11..15]
 
+puts "  Metodo que selecciona un alumno entre los grupos"
+puts "vemos el array de alumnos: "
+puts "----------------------------"
+puts alumnos
+print "__________________"
+puts "  "
+puts "Conformacion de los grupos"
+puts "##########################"
 puts "Grupo 1: #{group_one}"
 puts "Grupo 2: #{group_two}"
 puts "Grupo 3: #{group_three}"
-
+puts " "
+puts " Alumno conejillo de indias: "
 puts alumnos.sample
